02591adc3432104be72ac892739eb30bcf32bfd0
Van Nhan <nguyenvannhan0810@gmail.com>
2020-03-10 17:01:08 +0700

    Finish Lesson6 Component

---
+++ todo-list/src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoItem() {
+    return (
+        <div className="TodoItem">
+            <p>Di cho</p>
+            <p>Di cho</p>
+        </div>
+    );
+}
+
+export default TodoItem;
